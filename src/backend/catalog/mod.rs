177abc6d9f02de9dafffcc618949ca9d546b17e3
Michael Malis <michaelmalis2@gmail.com>
2026-04-11 11:52:12 -0700

    remove virtual core catalog scans

--- src/backend/catalog/mod.rs
+++ src/backend/catalog/mod.rs
@@ -5,7 +5,6 @@ pub mod pg_attribute;
 pub mod pg_class;
 pub mod pg_type;
 pub mod store;
-pub mod system_catalogs;
 
 pub use bootstrap::*;
 pub use catalog::*;
@@ -14,4 +13,3 @@ pub use pg_attribute::*;
 pub use pg_class::*;
 pub use pg_type::*;
 pub use store::*;
-pub use system_catalogs::*;
